de3795a6a5f47d0aaed2718681d12ee2c7d8b804
Nora Gully <nora.gully@gmail.com>
2018-02-22 10:24:57 -0700

    First test for apiCalls > resolveEndpoint passing

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,1 +1,24 @@
 import * as apiCalls from './apiCalls'
+
+describe('apiCalls', () => {
+  describe('resolveEndpoint', () => {
+    beforeAll(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          status: 200, 
+          json: () => { [
+            {name:"House Corbray of Heart's Home"}, 
+            {name: "House Dayne of Starfall"} ] }
+        })
+      })
+    })
+
+    it('should call fetch with the correct params', async () => {
+      const mockUrl = 'http://localhost:3001/api/v1/houses'
+      const response = await apiCalls.resolveEndpoint(mockUrl)
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+    })
+
+  
+  })
+})
