7d4b18db118a7d05fb2beb0b69d601609c6afe5e
Nora Gully <nora.gully@gmail.com>
2018-02-22 12:07:26 -0700

    Snapshot test passing in App.test.js

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -6,18 +6,32 @@ import { shallow } from 'enzyme'
 describe('App', () => {
 
   describe('what it looks like', () => {
-    // beforeAll(() => {
-    //   const mockHouses = [
-    //     { name: "House Dayne of Starfall"}, 
-    //     { name: "House Lannister of Casterly Rock"}
-    //   ]
-    //   let wrapper = shallow( <App houses={mockHouses} populateHouses={jest.fn()} /> )
-    // })
-    
-    it('should match snapshot', () => {
-      
-      // expect(wrapper).toMatchSnapshot();
+    beforeAll(() => {
+      const mockHouses = [
+        { name: "House Dayne of Starfall"}, 
+        { name: "House Lannister of Casterly Rock"}
+      ]
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve( {
+          status: 200, 
+          json: () => { Promise.resolve( mockHouses )}
+        })
+      })
     })
 
+    it('should match snapshot', () => {
+      const mockHouses = [
+        { name: "House Dayne of Starfall"}, 
+        { name: "House Lannister of Casterly Rock"}
+      ]
+      const wrapper = shallow( <App houses={mockHouses} populateHouses={jest.fn()} /> )
+      expect(wrapper).toMatchSnapshot();
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('should define value of houses prop for the App container via Redux store', () => {
+      
+    })
   })
 })
