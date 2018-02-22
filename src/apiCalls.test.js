27826bfc2cc984e1183d7ba10137f35f563c72e2
Nora Gully <nora.gully@gmail.com>
2018-02-22 10:32:17 -0700

    Second test passing for apiCalls

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -6,9 +6,10 @@ describe('apiCalls', () => {
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
           status: 200, 
-          json: () => { [
-            {name:"House Corbray of Heart's Home"}, 
-            {name: "House Dayne of Starfall"} ] }
+          json: () => Promise.resolve( [
+                    { name: "House Corbray of Heart's Home"}, 
+                    { name: "House Dayne of Starfall"} 
+                  ] )
         })
       })
     })
@@ -19,6 +20,19 @@ describe('apiCalls', () => {
       expect(window.fetch).toHaveBeenCalledWith(mockUrl)
     })
 
-  
+    it('should return an array of house objects if the status code is OK', async () => {
+      const mockUrl = 'http://localhost:3001/api/v1/houses'
+      const expected = [
+            {name:"House Corbray of Heart's Home"}, 
+            {name: "House Dayne of Starfall"} ]
+      const response = await apiCalls.resolveEndpoint(mockUrl)
+      expect(response).toEqual(expected)
+    })
+
+    it('should throw an error if the status code is not OK', () => {
+      
+
+    })
+
   })
 })
